3d1118e28b68dbc20d5c061aa6956250384918ca
Phoenix <anmolkhanna2003@gmail.com>
2023-03-03 15:46:50 +0530

    Code from 3rd March, Finding needle in a Haystack!

--- Find_the_Index_of_the_First_Occurrence_in_a_String.js
+++ Find_the_Index_of_the_First_Occurrence_in_a_String.js
@@ -1,13 +1,20 @@
 //Date 3rd March
 //Runtime: 52ms beats 97.7%, Memory: 41.2MB beats 98.94%;
+
+//Replacing .substring(begin,end) with a for loop
+
+//Runtime: 57ms beats 89.45%, Memory: 42.4MB beats 20.15%;
 const strStr = function (haystack, needle) {
-    const lenOfNeedle=needle.length;
-    const lenOfHaystack=haystack.length;
-    if(lenOfNeedle<=lenOfHaystack){
-        for (let i=0;i<lenOfHaystack;i++){
-            if(haystack.charAt(i)===needle.charAt(0) && (i-1)+lenOfNeedle<lenOfHaystack){
-                const subString=haystack.substring(i,i+lenOfNeedle);
-                if(subString===needle)return i;
+    if(needle.length<=haystack.length){
+        for(let i=0;i<haystack.length;i++){
+            if(haystack.charAt(i)===needle.charAt(0) && (i-1)+needle.length<haystack.length){
+                let j=0
+                for(;j<needle.length;j++){
+                    if(haystack[i+j]!==needle[j]){
+                        break;
+                    }
+                }
+                if(j===needle.length)return i;
             }
         }
     }
